f74b55c8a053438967c5b684a982ce79b99e9c56
Lars Wuckel <git@wuckel.name>
2019-02-09 12:07:31 +0100

    Monitor doesn't open the file. Only a link is shown.

--- GWT.Tests/MonitorLogFile.cs
+++ GWT.Tests/MonitorLogFile.cs
@@ -1,31 +1,26 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GWT.Tests
 {
 	class MonitorLogFile
 	{
 		private string logfile;
-		public MonitorLogFile Instance = null;
+		public static MonitorLogFile Instance = null;
 
 		public MonitorLogFile()
 		{
-			if (this.Instance == null)
+			if (Instance == null)
 			{
-				this.Instance = this;
+				Instance = this;
 				this.logfile = Path.Combine(TestContext.CurrentContext.TestDirectory, @"log.txt");
 				var stream = File.Create(logfile);
 				stream.Dispose();
 
 				Monitor.Instance.Processed += (args) =>
 					AddOutput($@"{ (args.Passed ? "PASSED" : "FAILED")} #{args.State}: {args.Name}
-{args.Exception?.ToString()}
+Exception: {args.Exception?.ToString()}
 ---------------------------");
 
 				Monitor.Instance.TestBegin += () =>
@@ -35,17 +30,11 @@ Scenario: {TestContext.CurrentContext.Test.ClassName}
 Begin: {TestContext.CurrentContext.Test.MethodName}");
 				};
 
-				bool openFile = true;
 				Monitor.Instance.TestEnd += () =>
 				{
 					AddOutput($@"End: {TestContext.CurrentContext.Test.FullName}
 ########################");
-
-					if (openFile)
-					{
-						openFile = false;
-						Process.Start(this.logfile);
-					}
+					Console.WriteLine($"Log is written to '{this.logfile}'");
 				};
 			}
 		}
