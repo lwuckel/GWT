edeade674c88a35e45c3916f337128813cb10e5b
Lars Wuckel <wuckel@hhk.de>
2019-02-22 12:00:11 +0100

    Exception wurde nicht im Test-Fenster angezeigt

--- GWT.NUnit3/GwtAssertException.cs
+++ GWT.NUnit3/GwtAssertException.cs
@@ -1,15 +1,33 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GWT.NUnit3
 {
 	public class GwtAssertException : AssertionException
 	{
-		public GwtAssertException(Exception[] exceptions) : base("")
+		public GwtAssertException(Exception[] exceptions) : base("", exceptions.FirstOrDefault())
 		{
-			this.Exceptions = exceptions;
+			this.Exceptions = exceptions;			
+		}
+
+		public override ResultState ResultState
+		{
+			get
+			{
+				return this.Exceptions.Where( e => e is AssertionException)
+					.Cast<AssertionException>()
+					.Select(ex => ex.ResultState)
+					.FirstOrDefault();
+			}
+		}
+
+		public override string StackTrace
+		{
+			get => GetOutout(ex => ex.StackTrace);
 		}
 
 		public Exception[] Exceptions
@@ -17,7 +35,17 @@ namespace GWT.NUnit3
 			get; private set;
 		}
 
+		public override string Message
+		{
+			get => ToString();
+		}
+
 		public override string ToString()
+		{
+			return GetOutout( ex => ex.ToString());
+		}
+
+		private string GetOutout(Func<Exception, string> text)
 		{
 			var builder = new StringBuilder($"{this.Exceptions.Length} Exceptions are collected");
 			builder.AppendLine();
@@ -25,9 +53,9 @@ namespace GWT.NUnit3
 
 			foreach (var exception in this.Exceptions)
 			{
-				builder.Append(exception);
+				builder.Append(text(exception));
 				builder.AppendLine();
-				builder.AppendLine();
+				builder.AppendLine("_____________________________________");
 			}
 
 			return builder.ToString();
